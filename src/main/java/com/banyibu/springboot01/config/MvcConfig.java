fb28cd6547f94342914e75ba618691bbb669ea44
guodengfeng <guodengfeng@58.com>
2022-02-24 16:37:16 +0800

    init

---
+++ src/main/java/com/banyibu/springboot01/config/MvcConfig.java
@@ -0,0 +1,7 @@
+package com.banyibu.springboot01.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+
+public class MvcConfig extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
+
+}
