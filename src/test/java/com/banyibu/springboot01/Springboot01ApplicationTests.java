1feeea157e1efd7ac9e50b601547b9db2d185b01
guodengfeng <guodengfeng@58.com>
2022-02-26 18:34:18 +0800

    整合数据访问层

--- src/test/java/com/banyibu/springboot01/Springboot01ApplicationTests.java
+++ src/test/java/com/banyibu/springboot01/Springboot01ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.banyibu.springboot01;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class Springboot01ApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void contextLoads() {
+
     }
 
 }
